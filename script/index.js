ac161c5d4b99a0e219b653f0343f1c2c674511bb
ellensallberg <Ellen.sallberg1@gmail.com>
2026-03-16 15:19:39 +0100

    JS lesson 1

--- script/index.js
+++ script/index.js
@@ -1,1 +1,17 @@
+console.log("Hello,World!");
 
+let firstName = "Ellen";
+let age = 21;
+let favColor = "green";
+
+console.log(`My name is ${firstName}`);
+console.log(`I am ${age} years old`);
+console.log(`My favorite color is ${favColor}`);
+
+let x = 100;
+let y = 5;
+
+console.log(x + y)
+console.log(x - y)
+console.log(x / y)
+console.log(x * y)
