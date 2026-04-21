2cb5904c26fd3b5077df916787417b5d162075fb
ellensallberg <Ellen.sallberg1@gmail.com>
2026-04-21 09:47:48 +0200

    JS Assignment 3

--- script/Assignment3.js
+++ script/Assignment3.js
@@ -1,1 +1,24 @@
 //Assignment 3
+function isValidPassword(password, username) {
+    
+    if (password.length < 8) {
+        console.log("your password isn't long enough");
+        return false;
+    }
+    if (password.includes(" ")) {
+        console.log("your password cannot include a space");
+        return false;
+    } 
+    if (password.includes(username)) {
+        console.log("your password cannot include your username");
+        return false;
+    } else {
+        console.log("your password is valid");
+        return true;
+    }
+}
+console.log(isValidPassword("pass", "myUserName"));
+console.log(isValidPassword("pass word", "myUserName"));
+console.log(isValidPassword("myUserName", "myUserName"));
+console.log(isValidPassword("myPassword", "myUserName"));
+
