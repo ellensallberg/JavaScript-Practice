28958d043a3f2903eedbbd3bba12ed6d47fe860a
ellensallberg <Ellen.sallberg1@gmail.com>
2026-05-06 08:54:02 +0200

    assignnment 4

---
+++ script/Assignment4.js
@@ -0,0 +1,14 @@
+const target = Math.floor(Math.random() * 10);
+let guess = null;
+
+while (guess !== target) {
+// console.log(`target: ${target}. {guess}`);
+guess = parseFloat(prompt("Make a guess"));
+if (guess > target) {
+    console.log (`${guess} is too high, try again`)
+}
+if (guess < target) {
+    console.log (`${guess} is too low, try again`)
+}
+}
+console.log(`the correct number was ${target}. Congrats you win!`);
