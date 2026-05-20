359fd09bae15c62a788b31b6eb4c8b6397326f8e
ellensallberg <Ellen.sallberg1@gmail.com>
2026-05-20 11:15:06 +0200

    Update Assignment5.js

--- script/Assignment5.js
+++ script/Assignment5.js
@@ -1,76 +1,86 @@
 let account = {
     accountName: "Ellen Sällberg",
-    balance: 0
-}
-console.log(account);
+    balance: 0,
 
-function atm() {
-    const message = parseFloat(
-        prompt("1.) See Balance 2.) Make a deposit 3.) Make a withdrawl 4.) Get account name 5.) Exit"
+    getBalance() {
+        console.log (account.balance)
+    },
+
+    deposit() { 
+        const depositAmount = parseFloat(
+        prompt("How much would you like to deposit?")
         )
-    );
-
-    switch (message) {
-        case 1:
-            function getBalance() {
-                console.log (account.balance)
-            }
-            getBalance();
-            atm();
-            break;
-
-        case 2:
-            function deposit() { 
-            const depositAmount = parseFloat(
-                prompt("How much would you like to deposit?")
-                )
-                if (depositAmount < 0) {
-                    console.log ("invalid amount")
-                }
-                else {
-                    account.balance = (account.balance + depositAmount);
-                }
-            }
-            deposit()
-            atm();
-            break;
-
-        case 3:
-            function withdrawl() { 
-            const withdrawlAmount = parseFloat(
-                prompt("How much would you like to withdrawl?")
-                )
-                if (withdrawlAmount < 0) {
-                    console.log ("invalid amount")
-                }
-                else {
-                    account.balance = (account.balance - withdrawlAmount);   
-                }
-            }
-            withdrawl()
-            atm();
-            break;
-
-        case 4:
-            function getAccountName() {
-                console.log (account.accountName)
-            }
-            getAccountName();
-            atm();
-            break;
-
-        case 5:
-            function logOut() {
-                console.clear();
-                console.log ("logged out")
+        if (isNaN(parseInt(depositAmount)) || depositAmount < 0) {
+            console.log ("invalid amount")
         }
-        logOut()
-        break;
+        else {
+            account.balance = (account.balance + depositAmount);
+        }
+    },
 
-        default:
+    withdrawl() { 
+        const withdrawlAmount = parseFloat(
+        prompt("How much would you like to withdrawl?")
+        )
+        if (isNaN(parseInt(withdrawlAmount)) || withdrawlAmount < 0 || withdrawlAmount > account.balance) {
+            console.log ("invalid amount")
+        }
+        else {
+            account.balance = (account.balance - withdrawlAmount);   
+        }
+    },
+
+    getAccountName() {
+        console.log (account.accountName)
+    },
+
+    logOut() {
+        running = false
+        console.clear()
+    },
+
+    deafult() {
         console.log ("invalid choice")
-        atm();
-        break;
     }
 }
+
+running = true
+
 atm()
+
+function atm() {
+
+    while (running) {
+
+        const message = parseFloat(
+            prompt("1.) See Balance 2.) Make a deposit 3.) Make a withdrawl 4.) Get account name 5.) Exit"
+            )
+        );
+
+        switch (message) {
+            case 1:
+                account.getBalance();
+                break;
+
+            case 2:
+                account.deposit();
+                break;
+
+            case 3:
+                account.withdrawl();
+                break;
+
+            case 4:
+                account.getAccountName();
+                break;
+
+            case 5:
+                account.logOut();
+                break;
+
+            default:
+                account.deafult();
+                break;
+        }
+    } 
+}
